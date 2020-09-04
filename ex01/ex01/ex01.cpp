6b5f2e3ec53c68be7a3c186723cbcafe41e62f21
MikkiSeidenschnur <mikki.seidenschnur@gmail.com>
2020-09-04 15:43:46 +0200

    Added assignment 1

---
+++ ex01/ex01/ex01.cpp
@@ -0,0 +1,76 @@
+// EXERCISE 1
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello World!" << endl;
+//
+//    return 0;
+//}
+
+// EXERCISE 2
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    string name;
+//
+//    cin >> name;
+//    cout << "Hello " + name + "!" << endl;
+//
+//    return 0;
+//}
+
+// EXERCISE 3
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int x;
+//    int y;
+//
+//    cin >> x;
+//    cin >> y;
+//
+//    if (x == y) {
+//        cout << x << " is equal to " << y << endl;
+//    }
+//
+//    else if (x > y) {
+//        cout << x << " is bigger than " << y << endl;
+//    }
+//
+//    else if (x < y) {
+//        cout << x << " is smaller than " << y << endl;
+//    }
+//
+//    return 0;
+//}
+
+// EXERCISE 4
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    float y;
+    float z;
+
+    cin >> x;
+    cin >> y;
+    cin >> z;
+
+    cout << (x + y) - z;
+
+    return 0;
+}
