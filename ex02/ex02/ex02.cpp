6374d3e7a3679fc7a27746f2c2e57cb9c7aaf5ce
MikkiSeidenschnur <mikki.seidenschnur@gmail.com>
2020-09-07 11:19:37 +0200

    added exercise 2, without solution on pi approximation

---
+++ ex02/ex02/ex02.cpp
@@ -0,0 +1,82 @@
+//EXERCISE 1
+//Gaussian sum
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int n;
+//    int sum = 0;
+//
+//    cin >> n;
+//
+//    for (int i = 0; i <= n; i++) {
+//
+//        sum += i;
+//    }
+//
+//    cout << sum << endl;
+//
+//}
+
+//EXERCISE 2
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	int sum = 0;
+//
+//	cin >> n;
+//
+//	for (int i = 0; i <= n; i++) {
+//		if (i % 2 == 0) {
+//			sum += i;
+//		}
+//		
+//	}
+//
+//	cout << sum << endl;
+//}
+
+//EXERCISE 3
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	int i = 2;
+//
+//	cin >> n;
+//
+//	while (n > 1) {
+//		if (n % i == 0) {
+//			n = n / i;
+//			cout << i;
+//			if (n > 1) {
+//				cout << " * ";
+//			}
+//		}
+//		else {
+//			i++;
+//		}
+//	}
+//	cout << endl;
+//}
+
+//EXERCISE 4
+#include <iostream>
+
+using namespace std;
+
+int
+
+
+
+
